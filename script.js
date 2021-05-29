85c3f47a99ac9e1e35f4be2a871b607d76926912
VVJS-22 <s.jayeshchennai@gmail.com>
2021-05-29 15:05:25 +0530

    second commit

--- script.js
+++ script.js
@@ -10,70 +10,70 @@ console.log("Hello World!");
 
 // var - function scope, variable
 
-var x;
-let y;
+// var x;
+// let y;
 
-x = 5;
-y = 10;
+// x = 5;
+// y = 10;
 
-let z = 8;
+// let z = 8;
 
-let i,j,k;
+// let i,j,k;
 
-let a=2, b=3, c=4;
+// let a=2, b=3, c=4;
 
-console.log(x);
-console.log(i);
+// console.log(x);
+// console.log(i);
 // console.log(h);
 
-x = 9;
-console.log(x);
+// x = 9;
+// console.log(x);
 
 // Naming variables
 
-let q = 5;
-let _q = 6;
-let $q = 7;
-let q1 = 8;
+// let q = 5;
+// let _q = 6;
+// let $q = 7;
+// let q1 = 8;
 
 // let 1q = 3;
 
-console.log(q,_q,$q,q1);
+// console.log(q,_q,$q,q1);
 // console.log(1q);
 
-console.log("a" == "a");
-console.log("a" === "a");
-console.log("a" == "A");
+// console.log("a" == "a");
+// console.log("a" === "a");
+// console.log("a" == "A");
 
-console.log(1 == 1);
-console.log(1 === 1);
-console.log(1 == "1");
-console.log(1 === "1");
-console.log("1" === "1");
+// console.log(1 == 1);
+// console.log(1 === 1);
+// console.log(1 == "1");
+// console.log(1 === "1");
+// console.log("1" === "1");
 
-console.log(1+1);
-console.log("1"+"1");
-console.log(1-1);
-console.log("1"-"1");
-console.log(1+"1")
-console.log(1-"1");
+// console.log(1+1);
+// console.log("1"+"1");
+// console.log(1-1);
+// console.log("1"-"1");
+// console.log(1+"1")
+// console.log(1-"1");
 
 // let new = 5;
 
-const r = 5;
-console.log(r);
+// const r = 5;
+// console.log(r);
 // r = 8;
 // console.log(r);
 // const s;
 
-const firstName = "Jayesh";
-const lastName = "Sugumaran";
+// const firstName = "Jayesh";
+// const lastName = "Sugumaran";
 
-console.log(firstName + lastName);
+// console.log(firstName + lastName);
 
-console.log(firstName + ' ' + lastName);
+// console.log(firstName + ' ' + lastName);
 
-console.log(`${firstName} ${lastName}`);
+// console.log(`${firstName} ${lastName}`);
 
 
 // Data          Types	Description	                                            Example
@@ -86,17 +86,17 @@ console.log(`${firstName} ${lastName}`);
 // Symbol	   data type whose instances are unique and immutable	   let value = Symbol('hello');
 // Object	   key-value pairs of collection of data	               let student = { };
 
-console.log('hello' - 'hi');
-console.log('3'-true);
-console.log(3+false);
-console.log(3-null);
+// console.log('hello' - 'hi');
+// console.log('3'-true);
+// console.log(3+false);
+// console.log(3-null);
 
 
-const name = "Jayesh";
-console.log(typeof(name));
+// const name = "Jayesh";
+// console.log(typeof(name));
 
-const body = null;
-console.log(typeof(body));
+// const body = null;
+// console.log(typeof(body));
 
-console.log(Number("3"));
-console.log(parseInt("3"));
+// console.log(Number("3"));
+// console.log(parseInt("3"));
